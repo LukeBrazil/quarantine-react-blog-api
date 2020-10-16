e6a16b1247083528a90b190cb1b5b46be8b81d34
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-16 09:11:39 -0500

    added get route for individual post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require('express');
+const cors = require('cors');
 
 const hostname = "127.0.0.1";
 const port = 3333;
@@ -8,10 +9,22 @@ const app = express();
 
 const server = http.createServer(app);
 
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept"
+  };
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 const rootController = require("./routes/index");
+
+app.use(cors(corsOptions));
 
 app.use('/', rootController);
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const blogModel = require('../models/blog');
 router.get('/', async (req,res) => {
     const blogData = await blogModel.getPosts();
     console.log(blogData);
-    res.json(blogData.status(200))
+    res.json(blogData).status(200)
 })
 
-router.get('/post', async (req,res) => {
+router.get('/post/:postId', async (req,res) => {
     const blogData = await blogModel.getSinglepost();
     console.log(blogData);
-    res.json(blogData.status(200))
+    res.json(blogData).status(200)
 })
 
 
